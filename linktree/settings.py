c59bb3c314d8f4be605cddba8e4ac572135c1c32
Zouss28 <alanjonse2@gmail.com>
2023-10-11 15:37:33 +0200

    add new host

--- linktree/settings.py
+++ linktree/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bigtree-23bcce19b107.herokuapp.com/']
 
 
 # Application definition
